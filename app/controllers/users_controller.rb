cbc35ae4f51e6afc642fcf85f2c83fec1428ced1
Oby98 <obaidurehman98@gmail.com>
2022-01-27 21:23:05 +0500

    enabled API for user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authorized, only: [:auto_login]
+  before_action :authorized, only: [:auto_login, :create]
 
   # REGISTER
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resource :users, only: [:create]
   post "/login", to: "users#login"
+  post "/create", to: "users#create"
   get "/auto_login", to: "users#auto_login"
 end
