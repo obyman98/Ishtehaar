d80c54c0c4c6845cbef1373eb538a7d3395db91a
Oby98 <obaidurehman98@gmail.com>
2022-02-02 02:43:15 +0500

    added module for email

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.signup_confirmation.subject
+
+  default from: "trans.ads.test@gmail.com"
+  def signup_confirmation(user)
+    @user = user
+
+    #mail to: "to@example.org"
+    mail to: user.email, subject: "Sign Up Confirmation"
+  end
+
+end
