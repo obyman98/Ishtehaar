89e4bc7be21c74b10743fd0b9817ed2368f55109
Oby98 <obaidurehman98@gmail.com>
2022-03-05 12:58:52 +0500

    smtp settings updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "trans.ads.test",
