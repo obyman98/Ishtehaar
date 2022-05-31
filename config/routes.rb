15f2975fef282a013591163a23a3b402caa88cb6
Oby98 <obaidurehman98@gmail.com>
2022-06-01 00:12:32 +0500

    Corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   post 'ads/rejected', to: 'ads#rejected'
   post 'ads/update', to: 'ads#update'
   post 'ads/edit', to: 'ads#edit'
-  post 'ads/edit', to: 'ads#assign'
+  post 'ads/assign', to: 'ads#assign'
   post 'vehicles/create', to: 'vehicles#create'
   post 'vehicles/pending', to: 'vehicles#pending'
   post 'vehicles/approved', to: 'vehicles#approved'
@@ -23,5 +23,5 @@ Rails.application.routes.draw do
   post 'companies/create', to: 'companies#create'
   post 'eld/create', to: 'elds#create'
   post 'eld/get', to: 'elds#get'
-  post 'eld/get', to: 'elds#show'
+  post 'eld/show', to: 'elds#show'
 end
